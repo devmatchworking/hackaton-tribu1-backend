197591815f6f9d9f19e967d7b0f614fa99ee475a
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-05-06 17:39:04 -0500

    fix: age field in UserInfo

--- api/models/user_info.py
+++ api/models/user_info.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field, EmailStr
 
 class UserInfo(BaseModel):
     name: str = Field(..., example="John Doe")
-    age: int = Field(..., gt=0, lt=130)
+    age: int = Field(..., gt=0, lt=130,example=19)
     email: EmailStr = Field(..., example="johndoe@example.com")
