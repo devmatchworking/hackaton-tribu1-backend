8917a6716fbec3c7843766e3a3895b17a62a86a1
jzafvt771 <ijmm7171@gmail.com>
2024-05-06 19:33:48 -0500

    fix: user_info validations

--- api/models/user_info.py
+++ api/models/user_info.py
@@ -3,12 +3,11 @@ from pydantic import BaseModel, Field, EmailStr
 # The UserInfo class is a Pydantic model that represents the data that the user will provide in the request body.
 # the atributes are used to generated a motivational letter
 class UserInfo(BaseModel):
-    name: str = Field(..., example="John Doe")
+    name: str = Field(..., min_length=5,max_length=25, example="John Doe")
     age: int = Field(..., gt=0, lt=130,example=19)
     email: EmailStr = Field(..., example="johndoe@example.com")
-    vacancy: str = Field(..., example="Software Engineer")
-    company: str = Field(..., example="ABC Company")
-    companyInfo: str = Field(..., example="ABC Company is a leading tech company in the industry")
-    experience: str = Field(..., example="3 years in backend development at XYZ Company")
+    vacancy: str = Field(..., min_length=5,max_length=50, example="Software Engineer")
+    company: str = Field(...,  min_length=5,max_length=25,example="ABC Company")
+    companyInfo: str = Field(...,  min_length=25,max_length=200, example="ABC Company is a leading tech company in the industry")
+    experience: str = Field(..., min_length=25,max_length=200, example="3 years in backend development at XYZ Company")
     
-
