b3b22d193d547994cd3679300f34569aa636297b
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-05-07 19:44:34 -0500

    fix: class letter_BD mongo related methods

--- api/models/letter_db.py
+++ api/models/letter_db.py
@@ -1,11 +1,21 @@
 from pydantic import BaseModel, Field, EmailStr,BaseConfig
 from models.letter import Letter
 from bson import ObjectId
+from letter import Letter
 
-class Letter_DB(BaseModel):
-    class Config(BaseConfig):
-        arbitrary_types_allowed = True
-    id: ObjectId
+
+class Letter_DB(Letter):
+    id: ObjectId = Field(..., example="6122a42e67a51d001555de9a")
     letter: Letter
     #date: 
     #user:
+
+    class Config:
+        arbitrary_types_allowed = True
+    
+    @classmethod
+    def from_mongo(cls, data):
+        return cls(**data)
+
+    def to_mongo(self):
+        return self.model_dump()
